400fcb55c5c8c63c95f94352a771187a36a09c9a
Ganesh Gowda <135863982+gowdaganesh005@users.noreply.github.com>
2024-07-10 13:33:30 +0000

    new container configuration

---
+++ basics/strings.js
@@ -0,0 +1,10 @@
+let name ="Ganesh"
+
+
+console.log(`this is a potent and new way to actually write a string .My name is ${name} and i am learning JS in a Week `);
+//string interpolation
+
+// using constructor
+
+let game= new String("cod")
+// string is not an array is like a key value pair as string is an object
