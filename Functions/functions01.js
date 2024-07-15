ae969d96adaf2cb3053d94fa7ce1f1decfabade4
Ganesh Gowda <135863982+gowdaganesh005@users.noreply.github.com>
2024-07-15 03:43:52 +0000

    basics 2 of functions in Js

--- Functions/functions01.js
+++ Functions/functions01.js
@@ -32,3 +32,41 @@ function call(name="user"){
 
 call("ganesh")
 call()   // is no arguments is passed then default is considered
+
+
+//passing unkmown number of elements in the functions
+
+function shoppingList(...num){     // here ... is the rest operator and is used earlier in spread operator
+
+    console.log(num)
+}
+
+shoppingList(300,2,222,53,654,776)
+
+function numlist(val1,val2,...num){
+   console.log(num)
+}
+numlist(556,55,34,53,43)  // here we have a llist but val1 is first ele and val2 is second so num is remaining
+
+numlist(45,67)
+
+// function avc(num1,num2){
+//     return num1,num2
+// }                             CANNOT return multiple values
+
+// let n1=avc(9,6)
+// console.log(n1)
+
+//passing objects in functions
+
+let user={
+    name:"gnaesh",
+    id_no:3029
+}
+
+function pirntobjdata(obj){
+    return `${obj.name} has game id of ${obj.id}`
+}
+
+console.log(pirntobjdata(user))      //type check need to be made so to avoid undefined value
+
